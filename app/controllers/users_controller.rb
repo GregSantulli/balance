5f9b97999c57e9a96f8f74aba12303deff6a4606
Greg Santulli <gregsantulli@gmail.com>
2016-09-22 18:02:00 -0400

    restyle expense input

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,10 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-
+    @group = current_user.groups.first
+    if @group
+      redirect_to group_path @group
+    end
   end
 
 end
